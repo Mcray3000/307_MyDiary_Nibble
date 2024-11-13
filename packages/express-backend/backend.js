f4f1d0ef3399f340f8150b1c1176fdf77a18969a
TLMatthies <timothy.l.matthies@gmail.com>
2024-11-13 11:42:11 -0800

    Saving changes for later

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import bcryptjs from "bcryptjs"
 dotenv.config({ path: "../../.env" });
 
 import { createClient } from "@supabase/supabase-js";
@@ -12,6 +13,19 @@ const supabaseUrl = "https://vzutkihkzjyhnwzqsgrx.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+async function check_login(user_name, password) {
+  const hash_to_check = await supabase
+    .from("users")
+    .select("password_hash")
+    .eq('user_name', user_name);
+  
+  const is_correct_password = bcryptjs
+    .compare(password, hash_to_check);
+
+  return is_correct_password
+};
+
 app.use(cors());
 
 app.use(express.json());
@@ -22,4 +36,18 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
+});
+
+app.get("/users", (req, res) => {
+  const user_name = req.body.user_name;
+  const password = req.body.password;
+
+  const was_successful = false;
+  check_login(user_name, password)
+    .then((result) => {
+      was_successful = result;
+    })
+    .catch((error) => {
+      console.log(error)
+    });
 });
