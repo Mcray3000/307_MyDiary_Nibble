3964d39c00a64df8b632f79b3860a0056e44d3e8
Mcray3000 <69219980+Mcray3000@users.noreply.github.com>
2024-11-24 21:10:51 -0800

    Made login work when you press enter

--- packages/react-frontend/src/Login.jsx
+++ packages/react-frontend/src/Login.jsx
@@ -12,6 +12,12 @@ function Login(props) {
 
   const navigate = useNavigate();
 
+  function handleKeyDown(event) {
+    if (event.key === "Enter") {
+      submitForm();
+    }
+  }
+
   function handleChange(event) {
     const { name, value } = event.target;
     setPerson((prevPerson) => ({
@@ -60,6 +66,7 @@ function Login(props) {
           id="password"
           value={person.password}
           onChange={handleChange}
+          onKeyDown={handleKeyDown}
           className="form-input"
           placeholder="password"
         />
