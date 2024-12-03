1047769f5267f1fb7b2f51e68f1a8544c5650fe6
Mcray3000 <69219980+Mcray3000@users.noreply.github.com>
2024-12-03 14:15:56 -0800

    spacing

--- packages/express-backend/routes/entries.js
+++ packages/express-backend/routes/entries.js
@@ -19,9 +19,9 @@ router.get("/home", authenticateUser, async (req, res) => {
       .select("id")
       .eq("user_name", req.user_name);
     if (userError) {
-      return res.status(500).send({ error: userError.message});
+      return res.status(500).send({ error: userError.message });
     }
-    const user_id = userData[0].id
+    const user_id = userData[0].id;
     const { data: entriesData, error: entriesError } = await supabase
       .from("entries")
       .select("*")
@@ -31,84 +31,84 @@ router.get("/home", authenticateUser, async (req, res) => {
     }
     res.status(200).send(entriesData);
   } catch (err) {
-    res.status.send({ error: "Server error "});
+    res.status.send({ error: "Server error " });
   }
 });
 
 //returns entry given an id
 router.get("/:entry_id", async (req, res) => {
-    try {
-      const { data, error } = await supabase
-        .from("entries")
-        .select("*")
-        .filter("entry_id", "eq", req.params["entry_id"]);
-      if (error) {
-        return res.status(500).send({ error: error.message });
-      }
-      res.status(200).send(data);
-    } catch (err) {
-      res.status(500).send({ error: "Server error" });
+  try {
+    const { data, error } = await supabase
+      .from("entries")
+      .select("*")
+      .filter("entry_id", "eq", req.params["entry_id"]);
+    if (error) {
+      return res.status(500).send({ error: error.message });
     }
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send({ error: "Server error" });
+  }
 });
 
 //used to look up all public entries
-  router.get("/", async (req, res) => {
-    const is_public = req.query.is_public ? req.query.is_public : "False";
-    try {
-      const { data, error } = await supabase.from("public_entries").select("*");
-      if (error) {
-        return res.status(500).send({ error: error.message });
-      }
-      console.log(data);
-      res.status(200).send(data);
-    } catch (err) {
-      res.status(500).send({ error: "Server error" });
+router.get("/", async (req, res) => {
+  const is_public = req.query.is_public ? req.query.is_public : "False";
+  try {
+    const { data, error } = await supabase.from("public_entries").select("*");
+    if (error) {
+      return res.status(500).send({ error: error.message });
     }
-  });
+    console.log(data);
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send({ error: "Server error" });
+  }
+});
 
-  router.put("/:entry_id", authenticateUser, async (req, res) => {
-    const { entry_id } = req.params;
-    const { title, entry, is_public, status } = req.body;
-  
-    try {
-      const { data, error } = await supabase
-        .from("entries")
-        .update({ title, entry, is_public, status })
-        .eq("entry_id", entry_id)
-        .select()
-        .single();
-  
-      if (error) {
-        return res.status(500).send({ error: error.message });
-      }
-      res.status(200).send(data);
-    } catch (err) {
-      console.log(err);
-      res.status(500).send({ error: "Server error" });
+router.put("/:entry_id", authenticateUser, async (req, res) => {
+  const { entry_id } = req.params;
+  const { title, entry, is_public, status } = req.body;
+
+  try {
+    const { data, error } = await supabase
+      .from("entries")
+      .update({ title, entry, is_public, status })
+      .eq("entry_id", entry_id)
+      .select()
+      .single();
+
+    if (error) {
+      return res.status(500).send({ error: error.message });
     }
-  });
-  
-  //adding a new entry
-  router.post("/", authenticateUser, async (req, res) => {
-    const { user_id, title, entry, is_public, status } = req.body;
-    // Set publish_date if status is 'published'
-    const publish_date = status === "published" ? new Date().toISOString() : null;
-    console.log(req.body);
-  
-    try {
-      const { data, error } = await supabase
-        .from("entries")
-        .insert({ user_id, title, entry, is_public, status, publish_date })
-        .select();
-  
-      if (error) {
-        return res.status(500).send({ error: error.message });
-      }
-      res.status(201).send(data);
-    } catch (err) {
-      console.log(err);
-      res.status(500).send({ error: "Server error" });
+    res.status(200).send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ error: "Server error" });
+  }
+});
+
+//adding a new entry
+router.post("/", authenticateUser, async (req, res) => {
+  const { user_id, title, entry, is_public, status } = req.body;
+  // Set publish_date if status is 'published'
+  const publish_date = status === "published" ? new Date().toISOString() : null;
+  console.log(req.body);
+
+  try {
+    const { data, error } = await supabase
+      .from("entries")
+      .insert({ user_id, title, entry, is_public, status, publish_date })
+      .select();
+
+    if (error) {
+      return res.status(500).send({ error: error.message });
     }
+    res.status(201).send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ error: "Server error" });
+  }
 });
 
 export default router;
