760773d89cee56a074ab7b3e62124db0f84e840e
Mcray3000 <69219980+Mcray3000@users.noreply.github.com>
2024-12-03 13:54:19 -0800

    rollback my backend changes for JohanathanXTims backend changes

--- packages/express-backend/routes/entries.js
+++ packages/express-backend/routes/entries.js
@@ -26,48 +26,11 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-//returns entry given an username
-router.get("/:username", async (req, res) => {
-  const { username } = req.params;
-  try {
-    // const { data: usersData, error: usersError } = await supabase
-    //   .from("users")
-    //   .select("id")
-    //   .eq("user_name", username);
-
-    // if (usersError) {
-    //   console.error("Error fetching user ID:", usersError);
-    //   return res.status(500).send({ error: usersError.message });
-    // }
-
-    // if (usersData.length === 0) {
-    //   return res.status(404).send({ error: "User not found" });
-    // }
-
-    // const userId = usersData[0].id;
-
-    const { data, error } = await supabase
-      .from("entries")
-      .select("*")
-      .eq("user_id", 125);
-    if (error) {
-      return res.status(500).send({ error: error.message });
-    }
-    console.log(data);
-    res.status(200).send(data);
-  } catch (err) {
-    res.status(500).send({ error: "Server error" });
-  }
-});
-
 //used to look up all public entries
 router.get("/", async (req, res) => {
   const is_public = req.query.is_public ? req.query.is_public : "False";
   try {
-    const { data, error } = await supabase
-      .from("entries")
-      .select("*")
-      .eq("user_id", 125);
+    const { data, error } = await supabase.from("public_entries").select("*");
     if (error) {
       return res.status(500).send({ error: error.message });
     }
@@ -82,7 +45,7 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   const { user_id, title, entry, is_public, status } = req.body;
   // Set publish_date if status is 'published'
-  const publish_date = new Date().toISOString();
+  const publish_date = status === "published" ? new Date().toISOString() : null;
   console.log(req.body);
 
   try {
@@ -95,28 +58,6 @@ router.post("/", async (req, res) => {
       return res.status(500).send({ error: error.message });
     }
     res.status(201).send(data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send({ error: "Server error" });
-  }
-});
-
-router.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const { title, entry, is_public, status } = req.body;
-
-  try {
-    const { data, error } = await supabase
-      .from("entries")
-      .update({ title, entry, is_public, status })
-      .eq("entry_id", id)
-      .select()
-      .single();
-
-    if (error) {
-      return res.status(500).send({ error: error.message });
-    }
-    res.status(200).send(data);
   } catch (err) {
     console.log(err);
     res.status(500).send({ error: "Server error" });
