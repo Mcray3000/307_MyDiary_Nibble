95d0c87e84c0f07b2d856da9dc9eb2e311e8bc85
J-Dees <jdees@outlook.com>
2024-12-03 08:39:54 -0800

    add McCay endpoint and backend auth function

---
+++ packages/express-backend/auth.js
@@ -0,0 +1,25 @@
+export function authenticateUser(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  //Getting the 2nd part of the auth header (the token)
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    console.log("No token received");
+    res.status(401).end();
+  } else {
+    jwt.verify(
+      token,
+      process.env.TOKEN_SECRET,
+      (error, decoded) => {
+        if (decoded) {
+          const user_name = decoded.id;
+          console.log(user_name);
+          next();
+        } else {
+          console.log("JWT error:", error);
+          res.status(401).end();
+        }
+      }
+    );
+  }
+}
--- packages/express-backend/routes/entries.js
+++ packages/express-backend/routes/entries.js
@@ -4,6 +4,7 @@ import express from "express";
 const router = express.Router()
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
+import { authenticateUser } from "../auth";
 dotenv.config({ path: "../../.env" });
 
 const supabaseUrl = "https://vzutkihkzjyhnwzqsgrx.supabase.co";
@@ -11,12 +12,12 @@ const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 //returns entry given an id
-router.get("/:id", async (req, res) => {
+router.get("/:entry_id", async (req, res) => {
     try {
       const { data, error } = await supabase
         .from("entries")
         .select("*")
-        .filter("entry_id", "eq", req.params["id"]);
+        .filter("entry_id", "eq", req.params["entry_id"]);
       if (error) {
         return res.status(500).send({ error: error.message });
       }
@@ -40,9 +41,35 @@ router.get("/:id", async (req, res) => {
       res.status(500).send({ error: "Server error" });
     }
   });
+
+  router.put("/:entry_id", authenticateUser, async (req, res) => {
+    const { entry_id } = req.params;
+    const { title, entry, is_public, status } = req.body;
+  
+    try {
+      const { data, error } = await supabase
+        .from("entries")
+        .update({ title, entry, is_public, status })
+        .eq("entry_id", entry_id)
+        .select()
+        .single();
+  
+      if (error) {
+        return res.status(500).send({ error: error.message });
+      }
+      res.status(200).send(data);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ error: "Server error" });
+    }
+  });
+
+  router.get("/home", authenticateUser, async (req, res) => {
+    return res.status(200).send();
+  })
   
   //adding a new entry
-  router.post("/", async (req, res) => {
+  router.post("/", authenticateUser, async (req, res) => {
     const { user_id, title, entry, is_public, status } = req.body;
     // Set publish_date if status is 'published'
     const publish_date = status === "published" ? new Date().toISOString() : null;
