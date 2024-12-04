424695b5e8d343f685bfe3b359ce7dfa192a7d15
jmgdls <jazzymae.delossantos@gmail.com>
2024-12-03 16:22:21 -0800

    Add log-in and user creation cypress tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,21 @@
 // ./cypress/e2e/login.cy.js
 
 describe('User login', () => {
+    it('User cannot log in with empty textboxes', () => {
+      //login page
+      cy.visit('http://localhost:5173/')
+
+      // submit the form 
+      cy.get('input.form-button[value="Login"]').click();
+
+      // error message
+      cy.contains('Invalid username or password').should('be.visible');
+      cy.url().should('eq', 'http://localhost:5173/');
+
+    })
+  })
+
+describe('User login', () => {
     it('User successfully logs in', () => {
       //login page
       cy.visit('http://localhost:5173/')
@@ -33,5 +48,40 @@ describe('User login', () => {
       // error message
       cy.contains('Invalid username or password').should('be.visible');
       cy.url().should('eq', 'http://localhost:5173/');
+    })
+  })
+
+  describe('User creation', () => {
+    it('User creates an account', () => {
+    
+    // cy.intercept('POST', '**', (req) => {
+    //     console.log(req);
+    //     }).as('allRequests');
+
+      // mock API call for user creation
+      cy.intercept('POST', '/users', {
+        statusCode: 201,
+        body: { message: 'User created successfully!' },
+      }).as('createUser');
+
+      //login page
+      cy.visit('http://localhost:5173/')
+
+      // click button to create a new user
+      cy.get('input.form-button[value="New User"]').click();
+
+      // fill in the form
+      cy.get('input[name="name"]').type('Cypress')
+      cy.get('input[type="password"]').type('Password')
+
+      // create user 
+      cy.get('input.form-button[value="Create"]').click();
+
+      // wait for mocked API call
+      cy.wait('@createUser').its('request.body').should('deep.equal', {
+        name: 'Cypress',
+        password: 'Password',
+      });
+
     })
   })
