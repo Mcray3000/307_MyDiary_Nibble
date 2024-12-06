223c832fb3df74fc88f780a89d2186574970c902
jmgdls <jazzymae.delossantos@gmail.com>
2024-12-05 19:34:21 -0800

    Implement FULL happy path for Cypress Testing

--- cypress/e2e/happy_path.cy.js
+++ cypress/e2e/happy_path.cy.js
@@ -45,11 +45,68 @@ describe('Happy path', () => {
       cy.get('input.diary-title').type('Public Test Entry');
       cy.get('input.diary-title').should('have.value', 'Public Test Entry');
 
+
       cy.get('textarea[placeholder="Scribble here..."]').type('Generates new public entry');
       cy.get('textarea[placeholder="Scribble here..."]').should('have.value', 'Generates new public entry');
       cy.get('button.diary-button').find('img[alt="Save"]').click();
 
       // navigates back to home/main page
       cy.url().should('eq', 'https://scribbleandnibble.vercel.app/main');
+      cy.contains('Public Test Entry').should('be.visible');
+
+      cy.get('a.create-button').click();
+      cy.get('input.diary-title').type('Private Test Entry');
+      cy.get('input.diary-title').should('have.value', 'Private Test Entry');
+
+      cy.get('textarea[placeholder="Scribble here..."]').type('Generates new private entry');
+      cy.get('textarea[placeholder="Scribble here..."]').should('have.value', 'Generates new private entry');
+      cy.get('button.diary-button').find('img[alt="Save"]').click();
+
+      cy.contains('Private Test Entry').should('be.visible');
+
+      // navigate to Public page
+      cy.get('button.hamburger-button').find('img[alt="Menu"]').click();
+      cy.contains('a', 'Public Scribbles').click();
+      cy.contains('Public Test Entry').should('be.visible');
+      cy.contains('Private Test Entry').should('not.exist');
+
+      // others entries are view only
+      cy.get('a[href="/discover/11"]').click();
+      cy.url().should('eq', 'https://scribbleandnibble.vercel.app/discover/11');
+      cy.get('input[type="text"]').should('not.exist');
+
+      // navigate to Calendar page
+      cy.get('button.hamburger-button').find('img[alt="Menu"]').click();
+      cy.contains('a', 'Calendar').click();
+      cy.url().should('eq', 'https://scribbleandnibble.vercel.app/calendar');
+      cy.get('button.round-button').eq(1).click();
+      cy.wait(500);
+      cy.contains('button', '5').click();
+      cy.contains('Calendar Test').should('be.visible');
+
+      // view page only
+      cy.get('a[href="/discover/112"]').click();
+      cy.url().should('eq', 'https://scribbleandnibble.vercel.app/discover/112');
+      cy.get('input[type="text"]').should('not.exist');
+
+      // navigate to home page
+      cy.get('button.hamburger-button').find('img[alt="Menu"]').click();
+      cy.contains('a', 'Home').click();
+      cy.url().should('eq', 'https://scribbleandnibble.vercel.app/main');
+      cy.contains('div', 'Public Test Entry').click();
+      cy.get('button.diary-button').find('img[alt="Trash"]').click();
+
+      cy.contains('div', 'Private Test Entry').click();
+      cy.get('button.diary-button').find('img[alt="Trash"]').click();
+      cy.wait(500);
+
+      cy.contains('Private Test Entry').should('not.exist');
+      cy.contains('Public Test Entry').should('not.exist');
+
+      // logout and bring you to login page
+      cy.get('button.hamburger-button').find('img[alt="Menu"]').click();
+      cy.contains('a', 'Logout').click();
+      cy.url().should('eq', 'https://scribbleandnibble.vercel.app/');
+
     })
   })
